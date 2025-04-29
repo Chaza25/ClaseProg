b91522cbde0fdd4eedbb6cf763e423b97ac7876c
Agustin Chazarreta Cruz <aguschacruz@gmail.com>
2025-04-29 20:47:46 -0300

    Seguimos probando

--- src/componentes/Main.jsx
+++ src/componentes/Main.jsx
@@ -1,10 +1,13 @@
+import React from "react"
+//import Button from 'react-bootstrap/Button';
+
 const Main = () => {
     return(
         <div className="bg-secondary">
             <br />
             <h3>Probando React</h3>
 
-
+            <button>Boton de prueba</button>
             <br />
         </div>
     )
